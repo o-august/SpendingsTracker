fcb003428266925642a1fc98622621f58b10ef04
o-august <aleksander.augustynowicz@gmail.com>
2023-06-12 20:36:45 +1000

    added date picker

--- App.js
+++ App.js
@@ -4,6 +4,9 @@ import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import CurrencyInput from 'react-native-currency-input';
+import DateTimePicker from '@react-native-community/datetimepicker';
+
+
 
 
 function HomeScreen() {
@@ -11,6 +14,7 @@ function HomeScreen() {
     <View style={styles.container}>
       <Text>Enter your spendings:</Text>
       <MoneyInput/>
+      <DateInput/>
       <StatusBar style="auto" />
     </View>
   );
@@ -47,9 +51,42 @@ function MoneyInput() {
   }
 
   return <> 
-  <CurrencyInput prefix="$" separator="." delimiter="," value={value} onChangeValue={setValue} />
+  <CurrencyInput prefix="$" separator="." delimiter="," value={value} onChangeValue={setValue} style= {styles.currencyStyle}/>
   <Button title='submit' onPress={submit}></Button>
   </>;
+}
+function DateInput(){
+  const [date, setDate] = React.useState(new Date(1598051730000));
+
+  const onChange = (event, selectedDate) => {
+    const currentDate = selectedDate;
+    setDate(currentDate);
+  };
+
+  const showMode = (currentMode) => {
+    DateTimePickerAndroid.open({
+      value: date,
+      onChange,
+      mode: currentMode,
+      is24Hour: true,
+    });
+  };
+
+  const showDatepicker = () => {
+    showMode('date');
+  };
+
+  const showTimepicker = () => {
+    showMode('time');
+  };
+
+  return (
+    <>
+      <Button onPress={showDatepicker} title="Show date picker!" />
+      <Button onPress={showTimepicker} title="Show time picker!" />
+      <Text>selected: {date.toLocaleString()}</Text>
+    </>
+  );
 }
 
 const styles = StyleSheet.create({
@@ -59,4 +96,11 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  currencyStyle: {
+    borderColor: "#111", 
+    borderWidth: 1,
+    paddingLeft: 45,
+    paddingRight: 45,
+    margin: 10
+  }
 });
