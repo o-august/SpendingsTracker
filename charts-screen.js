8a63a4c20964d6bdb2ed1bbfceff8173359858d9
o-august <aleksander.augustynowicz@gmail.com>
2023-06-26 22:28:48 +0200

    reading val. from database attempt 1

--- charts-screen.js
+++ charts-screen.js
@@ -1,11 +1,11 @@
 import { Text, View } from 'react-native';
 import { styles } from "./components/styles"
-import { getMonthTotal } from './components/database';
+import { getMonthTotalSync } from './components/database';
 
 export function ChartsScreen() {
     return (
       <View style={styles.container}>
-        <Text>{getMonthTotal()}</Text>
+        <Text>{getMonthTotalSync()}</Text>
       </View>
     );
   }
--- components/database.js
+++ components/database.js
@@ -3,14 +3,22 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 export const addSpending = async (money, date, category) => {
       const newSpending = {"money":money,"date":date,"category":category}
       var allSpendings = await AsyncStorage.getItem('@spendings')
-      allSpendings != null ? JSON.parse(allSpendings) : []
+      allSpendings = allSpendings != null ? JSON.parse(allSpendings) : []
       allSpendings.push(newSpending)
       const jsonValue = JSON.stringify(allSpendings)
       await AsyncStorage.setItem('@spendings', jsonValue);
   };
 
-
-export function getMonthTotal(){
-    return 99
-}
-
+export const getMonthTotal = async () => {
+    try {
+        const jsonValue = await AsyncStorage.getItem('@spendings');
+        console.log(jsonValue)
+      return jsonValue != null ? JSON.parse(jsonValue) : null;
+    } catch (e) {
+    }
+  };
+export function getMonthTotalSync(){
+    (async () => {
+        await getMonthTotal();
+    })();
+  }
