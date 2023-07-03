4d103085698bc8835768b490e780d22b2387a3a7
o-august <aleksander.augustynowicz@gmail.com>
2023-07-03 22:58:11 +0200

    added first chart with hardcoded values

--- charts-screen.js
+++ charts-screen.js
@@ -1,23 +1,62 @@
-import { Text, View } from 'react-native';
+import { Text, View, Dimensions } from 'react-native';
 import { styles } from "./components/styles"
 import { getMonthTotalSync, getMonthTotal } from './components/database';
 import * as React from 'react';
+import {
+  LineChart,
+  BarChart,
+  PieChart,
+  ProgressChart,
+  ContributionGraph,
+  StackedBarChart
+} from "react-native-chart-kit";
 
 export function ChartsScreen() {
-   
-    const [ totalAmount, updateTotalAmount ] = React.useState("5")
-   
-    React.useEffect(() => {
-        async function fetchData() {
-            const res = await getMonthTotal();
-            updateTotalAmount(res)
-        }
-        fetchData();
-    },[])
 
-    return (
-      <View style={styles.container}>
-        <Text>{totalAmount}</Text>
-      </View>
-    );
-  }
+  const [totalAmount, updateTotalAmount] = React.useState("5")
+
+  React.useEffect(() => {
+    async function fetchData() {
+      const res = await getMonthTotal();
+      updateTotalAmount(res)
+    }
+    fetchData();
+  }, [])
+
+  const data = {
+    labels: ["January", "February", "March", "April", "May", "June"],
+    datasets: [
+      {
+        data: [20, 45, 28, 80, 99, 43]
+      }
+    ]
+  };
+  const chartConfig = {
+    backgroundGradientFrom: "#1E2923",
+    backgroundGradientFromOpacity: 0,
+    backgroundGradientTo: "#08130D",
+    backgroundGradientToOpacity: 0.5,
+    color: (opacity = 1) => `rgba(26, 255, 146, ${opacity})`,
+    strokeWidth: 2, // optional, default 3
+    barPercentage: 0.5,
+    useShadowColorFromDataset: false // optional
+  };
+
+  return (
+    <View style={styles.container}>
+      <Text>{totalAmount}</Text>
+      <BarChart
+        style={{
+          marginVertical: 8,
+          borderRadius: 16
+        }}
+        data={data}
+        width={Dimensions.get("window").width}
+        height={220}
+        yAxisLabel="$"
+        chartConfig={chartConfig}
+        verticalLabelRotation={30}
+      />
+    </View>
+  );
+}
